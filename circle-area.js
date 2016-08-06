270ebac6f50f4932606ba71d9a3483b96d84fd67
Jitesh-ADAJ <jiteshjp@gmail.com>
2016-08-06 18:11:59 +0530

    Add files via upload

---
+++ circle-area.js
@@ -0,0 +1,12 @@
+
+
+function calcCircleArea(r){
+	
+	
+    	var r;  	
+
+    	x =3.14*r*r;
+    			
+		document.getElementById("p1").innerHTML = " The area of circle for radius "+r+ " is :"+x;		
+	}
+
